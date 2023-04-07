1667fa735d2af7b3f21bb53567b6b27157d1b1f9
Thad Riley <7145645+rthadr@users.noreply.github.com>
2023-04-07 17:07:01 -0500

    Switch to using flake parts

--- flake.nix
+++ flake.nix
@@ -1,23 +1,75 @@
-{
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+#
+# This flake will install several command line tools
+# used by devs. How do I use this file, you ask?
+#
+# 1. Install the nix package manager.
+#
+#        sh <(curl -L https://nixos.org/nix/install) --no-daemon
+#    
+#    This may require a restart of your shell.    
+#
+# 2. Enable nix flakes and experimental tooling.
+#
+#        mkdir -p ~/.config/nix && \
+#        echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+#
+# 3. Run `nix profile install .` to install the packages.
+#
 
-  outputs = inputs:
-    let
-      inherit (inputs) nixpkgs flake-utils;
-    in
-      flake-utils.lib.eachDefaultSystem (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          defaultPackage = pkgs.buildEnv {
-            name = "default profile";
+{
+  description = "Base Developer Environment";
+
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+
+  outputs = inputs@{ self, flake-parts, nixpkgs }:
+
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      #
+      # Here we can list the systems we are supporting
+      #
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = { pkgs, system, ... }: {
+        packages = {
+
+          #
+          # buildEnv is a convenient derivation container for a list of packages.
+          #
+          base = pkgs.buildEnv {
+            name = "dev-base";
             paths = with pkgs; [
-              git
               direnv
+              git
               nix-direnv
             ];
           };
-        }
-      );
+
+          #
+          # In order to create a set of additonal packages
+          # we can add an additional output here with a
+          # similar structure to packages.base, named "extras".
+          # We can then install it with "nix profile install '.#extras'".
+          #
+          extras = pkgs.buildEnv {
+            name = "dev-extras";
+            paths = with pkgs; [
+              ripgrep
+              starship
+            ];
+          };
+          
+          #
+          # packages.default must be set to a derivation
+          #
+          default = self.packages.${system}.base;
+        };
+      };
+    };
 }
